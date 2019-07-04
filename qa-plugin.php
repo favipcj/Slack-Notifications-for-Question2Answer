4694f538998a27fa494b2e47b116c8a2f4fcf777
Emrah Onder <emrah@nioya.com>
2019-07-04 20:32:11 +0300

    Initial Commit

---
+++ qa-plugin.php
@@ -0,0 +1,18 @@
+<?php
+/*
+	Plugin Name: Slack Notifications
+	Plugin URI: https://www.nioya.com/
+	Plugin Description: Sends Slack notifications of new questions.
+	Plugin Version: 0.1
+	Plugin Date: 2019-04-07
+	Plugin Author: nioya
+	Plugin Author URI: https://github.com/nioya
+	Plugin License: MIT
+	Plugin Minimum Question2Answer Version: 1.5
+*/
+if (!defined('QA_VERSION')) { // don't allow this page to be requested directly from browser
+    header('Location: ../../');
+    exit;
+}
+qa_register_plugin_module('event', 'qa-slack-notifications-event.php', 'qa_slack_notifications_event', 'Slack Notifications');
+qa_register_plugin_module('page', 'qa-slack-notifications-page.php', 'qa_slack_notifications_page', 'Slack Notifications Configuration');
